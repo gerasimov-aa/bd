f5ae2147215b09aaf039e9f8f060938d0df158ca
Anton <151626877+gerasimov-aa@users.noreply.github.com>
2025-12-18 09:59:21 +0500

    Update settings.py

--- cert_manager/cert_manager/settings.py
+++ cert_manager/cert_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@&2&@q^a07bl%vq+8=75_ewoxejqme=c2a8@!1j3@u8nb5-2hd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.214', '127.0.0.1']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
